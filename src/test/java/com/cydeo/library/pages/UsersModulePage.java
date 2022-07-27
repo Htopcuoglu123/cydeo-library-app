4d102a6edcfc4306d5dfe7c653f6d0732971cbfe
Htopcuoglu <httopcuoglu@gmail.com>
2022-07-26 22:26:53 -0400

    UsersPage edited

--- src/test/java/com/cydeo/library/pages/UsersModulePage.java
+++ src/test/java/com/cydeo/library/pages/UsersModulePage.java
@@ -1,6 +1,7 @@
 package com.cydeo.library.pages;
 
 import com.cydeo.library.utilities.Driver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -8,6 +9,19 @@ public class UsersModulePage extends BasePage{
     public UsersModulePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    @FindBy(partialLinkText =" Add User")
+    WebElement addUserBtn;
 
+    @FindBy(name = "full_name")
+    WebElement fullNameBox;
+
+    @FindBy(name = "password")
+    WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@placeholder='Email']")
+    WebElement emailBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement saveChangesBtn;
 
 }
